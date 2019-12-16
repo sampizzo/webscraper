116c499530ffda0ef1a8be93adca5ecc4f9093de
SP <sampizzo@gmail.com>
2019-12-16 02:58:24 -0500

    Models setup, Article.js

---
+++ models/Article.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+var ArticleSchema =  new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    link: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    img: {
+        type: String
+    },
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
+})
